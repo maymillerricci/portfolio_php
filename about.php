2dee6dd05351f3b4ab3d67032ac1978a2e798687
May Miller-Ricci <maymillerricci@gmail.com>
2013-11-30 15:27:42 -0800

    add home page text and ninja icons, update about page

--- about.php
+++ about.php
@@ -1,32 +1,29 @@
 <?php include("template_header.php"); ?>
 
-	<a href="http://www.codingdojo.co">
-		<img src="assets/images/red.png" alt="red belt ninja">
-	</a>
-	<p>Hello! My name is May, and I'm currently learning web development at <a href="http://www.codingdojo.co">Coding Dojo</a> programming bootcamp. Here are some links about me:</p>
+	
 
 	<a href="http://www.maymillerricci.wordpress.com">
 		<img src="assets/images/stars.jpg" alt="moon and stars blog icon"></a>
-	<p><a href="http://www.maymillerricci.wordpress.com">My blog: The Coding Road</a> - Read my blog about learning to code and attending Coding Dojo bootcamp.</p>
+	<p class="fix_height"><a href="http://www.maymillerricci.wordpress.com">My blog: The Coding Road</a> - Read my blog about learning to code and attending Coding Dojo bootcamp.</p>
 	
 	<a href="http://www.linkedin.com/in/maymillerricci">
-		<img src="assets/images/linkedin.jpg" alt="LinkedIn logo">
+		<img src="assets/images/linkedin.png" alt="LinkedIn logo">
 	</a>
-	<p><a href="http://www.linkedin.com/in/maymillerricci">LinkedIn</a> - View my professional profile.</p>
+	<p class="fix_height"><a href="http://www.linkedin.com/in/maymillerricci">LinkedIn</a> - View my professional profile.</p>
 	
 	<a href="https://github.com/maymillerricci">
 		<img src="assets/images/octocat.png" alt="Github logo">
 	</a>
-	<p><a href="https://github.com/maymillerricci">Github</a> - Check out my code!</p>
+	<p class="fix_height"><a href="https://github.com/maymillerricci">Github</a> - Check out my code!</p>
 
 	<a href="http://www.codeivate.com/users/maymillerricci">
 		<img src="http://www.codeivate.com/users/maymillerricci/signature.jpg" alt="Codeivate signature">
 	</a>
-	<p><a href="http://www.codeivate.com/users/maymillerricci">Codeivate</a> - Watch me "level up" as I code.</p>
+	<p class="fix_height"><a href="http://www.codeivate.com/users/maymillerricci">Codeivate</a> - Watch me "level up" as I code.</p>
 	
 	<a href="http://www.ravelry.com/people/maymay7">
 		<img src="assets/images/knit.png" alt="wizard icon">
 	</a>
-	<p><a href="http://www.ravelry.com/people/maymay7">Ravelry</a> - When I'm not coding, I love to knit! Check me out on Ravelry if you're a fellow Ravelry crafter.</p>
+	<p class="fix_height"><a href="http://www.ravelry.com/people/maymay7">Ravelry</a> - When I'm not coding, I love to knit! Check me out on Ravelry if you're a fellow Ravelry crafter.</p>
 
 <?php include("template_footer.php"); ?>
--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php include("template_header.php"); ?>
 
+	<h2>Welcome!</h2>
+
+	<img class="ninja" src="assets/images/yellow.png" alt="yellow belt ninja">
+	<img class="ninja" src="assets/images/green.png" alt="green belt ninja">
+	<img class="ninja" src="assets/images/red.png" alt="red belt ninja">
+	<img class="ninja" src="assets/images/black.png" alt="black belt ninja">
+	
+	<p class="clear">My name is May, and I'm currently learning web development at <a href="http://www.codingdojo.co">Coding Dojo</a> programming bootcamp.  I'm learning HTML, CSS, jQuery, JavaScript, PHP, MySQL, Ajax, OOP, MVC, CodeIgniter, Ruby on Rails, and more.</p>
+	<p class="clear">Check out the <a href="about.php">About Me</a> section for some links about me, read my <a href="http://www.maymillerricci.wordpress.com">Blog</a>, or take a look at the projects in my <a href="portfolio.php">Portfolio</a>.</p>
+	<p class="clear">Thanks for stopping by!</p>
+
+
+	
+
+
 <?php include("template_footer.php"); ?>
