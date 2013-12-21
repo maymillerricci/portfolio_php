84d0fd8e390af7b66e6a4282406a85358cfd8d47
May Miller-Ricci <maymillerricci@gmail.com>
2013-12-20 18:59:30 -0800

    add pictures to portfolio

--- portfolio.php
+++ portfolio.php
@@ -1,14 +1,42 @@
 <?php include("template_header.php"); ?>
 
 	<h3 class="project_title">Canvas Drawing:</h3>
-	<p class="project_link"><a href="http://www.maymillerricci.com/canvas_draft1.html" target="_blank">First Draft of Drawing App</a></p>
-	<p class="project_explanation">Created using HTML5 canvas, jQuery, jQuery UI, and jQuery mobile to also work on touchscreen devices.</p>
-
+	<div class="fix_height">
+		<a href="http://mathcanvas.herokuapp.com/" target="_blank">
+			<img class="to_sepia no_margin_top" src="assets/images/mathcanvaslogo.png" alt="math canvas logo">
+		</a>
+		<p class="project_link"><a href="http://mathcanvas.herokuapp.com" target="_blank">Math Canvas</a></p>
+		<p class="project_explanation">Site where students can draw out solutions to math problems, using artwork to illustrate their individual thinking processes.  Created in Ruby on Rails, integrating my HTML5 canvas and jQuery drawing app.</p>
+	</div>
+	
+	<div class="fix_height">
+		<a href="http://www.maymillerricci.com/canvas_draft1.html" target="_blank">
+			<img class="to_sepia no_margin_top" src="assets/images/canvas.png" alt="canvas">
+		</a>
+		<p class="project_link"><a href="http://www.maymillerricci.com/canvas_draft1.html" target="_blank">First Draft of Drawing App</a></p>
+		<p class="project_explanation">Created using HTML5 canvas, jQuery, jQuery UI, and jQuery mobile to also work on touchscreen devices.</p>
+	</div>
+	
 	<h3 class="project_title">Here are a few of my favorite assignments from Coding Dojo:</h3>
-	<p class="project_link"><a href="projects/periodic_table"target="_blank">Periodic Table of the Elements</a></p>
-	<p class="project_explanation">Recreated from scratch using HTML, CSS, and jQuery.  Hover over each element to see its name and <a href="http://jmol.sourceforge.net/jscolors/#Jmol colors" target="_blank">JMOL color</a>.</p>
 
-	<p class="project_link"><a href="projects/countries"target="_blank">Countries of the World</a></p>
-	<p class="project_explanation">Country information form using Ajax.</p>
+
+	<div class="fix_height">
+		<a href="projects/periodic_table" target="_blank">
+			<img class="to_sepia no_margin_top" src="assets/images/periodic_table.png" alt="periodic table">
+		</a>
+		<p class="project_link"><a href="projects/periodic_table" target="_blank">Periodic Table of the Elements</a></p>
+		<p class="project_explanation">Recreated from scratch using HTML, CSS, and jQuery.  Hover over each element to see its name and <a href="http://jmol.sourceforge.net/jscolors/#Jmol colors" target="_blank">JMOL color</a>.</p>
+	</div>
+
+	<div class="fix_height">
+		<a href="projects/countries" target="_blank">
+			<img class="to_sepia no_margin_top" src="assets/images/earth.png" alt="country info">
+		</a>
+		<p class="project_link"><a href="projects/countries" target="_blank">Countries of the World</a></p>
+		<p class="project_explanation">Country information form using Ajax.</p>
+	</div>
 
 <?php include("template_footer.php"); ?>
+
+
+
